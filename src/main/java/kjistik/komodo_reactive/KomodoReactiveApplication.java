a94c7ffc83bdb5997f9bec0118e9229306e77c78
kjistik <coronelmartinernesto@gmail.com>
2024-09-19 16:15:02 -0300

    first commit

---
+++ src/main/java/kjistik/komodo_reactive/KomodoReactiveApplication.java
@@ -0,0 +1,13 @@
+package kjistik.komodo_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomodoReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomodoReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kjistik/komodo_reactive/KomodoReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package kjistik.komodo_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomodoReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
